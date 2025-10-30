d54770f50835c7d09c575e19bb4b639d456612b7
josevasquezramos <jmvr16092003@gmail.com>
2025-10-30 18:43:13 -0500

    fix: Corregir rutas de publicación de dependencias

--- src/PackTestServiceProvider.php
+++ src/PackTestServiceProvider.php
@@ -38,9 +38,9 @@ class PackTestServiceProvider extends PackageServiceProvider
                 __DIR__ . '/../database/migrations/add_columns_to_users_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_add_columns_to_users_table.php'),
             ], 'pack-test-migrations');
             $this->publishes([
-                __DIR__ . '/../vendor/spatie/laravel-permission/config/permission.php' => config_path('permission.php'),
-                __DIR__ . '/../vendor/spatie/laravel-permission/database/migrations/create_permission_tables.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 1) . '_create_permission_tables.php'),
-                __DIR__ . '/../vendor/laravel/sanctum/database/migrations/create_personal_access_tokens_table.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 2) . '_create_personal_access_tokens_table.php'),
+                base_path('vendor/spatie/laravel-permission/config/permission.php') => config_path('permission.php'),
+                base_path('vendor/spatie/laravel-permission/database/migrations/create_permission_tables.php.stub') => database_path('migrations/' . date('Y_m_d_His', time() + 1) . '_create_permission_tables.php'),
+                base_path('vendor/laravel/sanctum/database/migrations/create_personal_access_tokens_table.stub') => database_path('migrations/' . date('Y_m_d_His', time() + 2) . '_create_personal_access_tokens_table.php'),
             ], 'pack-test-dependencies');
         }
     }
